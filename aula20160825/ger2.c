76233e58143dad0cd8edbf3e044634cb440ff411
kellendl <kellenduarte13@hotmail.com>
2016-08-26 14:17:17 -0300

    fim de aula

--- aula20160825/ger2.c
+++ aula20160825/ger2.c
@@ -2,35 +2,38 @@
 #include<time.h>
 int main()
 {
-    srand(time (0));
-    int numero, numero_sorteado,i;
-    printf("\nEscolha um numero no intervalo de 0 a 100:");
-    scanf("%d",&numero);
+    srand (time (0));
+    int numero,numero_sorteado,i=0;
 
-    numero_sorteado=("%d", rand()%100);
-    printf("\nNumero sorteado:%d", numero_sorteado);
+    numero_sorteado = ("%d", rand()%100);
+
 
     do
+    {
+        printf("\n Informe um numero de 0 a 99 \n");
+        scanf("%d",&numero);
 
-    if(0<=numero<100)
-    {
-        if(numero<numero_sorteado)
-    {
-        printf("BAIXO!!");
-        break;
-    }
-    else if (numero>numero_sorteado)
-    {
-        printf("ALTO");
-        break;
-    }
-    else
-    {
-        printf("ACERTOU!!!");
-        break;
-    }
-    }
+        if(0<=numero<100)
+            {
+                if (numero<numero_sorteado)
+                {
+                    printf("\n BAIXO! \n");
+                    i++;
+                }
+                else
+                    if (numero>numero_sorteado)
+                    {
+                        printf("\n ALTO! \n");
+                        i++;
+                    }
+                    else
+                        {
+                            printf("\n ACERTOU! \n");
+                            break;
+                        }
+            }
+    } while (numero!=numero_sorteado);
 
-    while(numero!=numero_sorteado);
+    printf("\n Numero de tentativas %d\n",i+1);
     return 0;
 }
